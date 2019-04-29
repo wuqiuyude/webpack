2bdb63139652975fa1cb3be5d1de95190974b232
wuqiuyu <wuqiuyu@lukou.com>
2019-04-29 10:28:35 +0800

    fix vuex

--- template/src/main.js
+++ template/src/main.js
@@ -9,9 +9,7 @@ import App from './App'
 import router from './router'
 {{/router}}
 {{#vuex}}  //vuex为true的时候就会写入这些
-import Vuex from 'vuex'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 import store from  './vuex/store'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-Vue.use(Vuex){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 {{/vuex}}
 {{#yx-vue-ui}} 
 import YxUi from 'yx-vue-ui-lib'
